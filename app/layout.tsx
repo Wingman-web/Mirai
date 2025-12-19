1e46528c8330bb86bfa908d60b1741bdce554b2a
rahul_singhsss <rs913050@gmail.com>
2025-12-19 17:45:20 +0530

    chore: add favicon files and link from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ const century = localFont({
 });
 
 import NavContainer from "@/components/Home/Navbar/NavContainer";
+import logoImg from "../components/Helper/logo.png";
 
 // Using system font 'Century Gothic' instead of Geist.
 
@@ -39,6 +40,10 @@ export default function RootLayout({
       <head>
         {/* Playfair Display (headings) still loaded from Google */}
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet" />
+        {/* Preferred favicons in public/ to satisfy browsers requesting /favicon.ico */}
+        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
       </head>
       <body className={`${migra.variable} ${century.variable} antialiased `}>
         <NavContainer />
